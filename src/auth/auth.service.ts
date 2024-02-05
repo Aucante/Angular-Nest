b556ea97bd28f9f27564dc827a2b93420acc4eba
Aucante <sebastien.aucante@gmail.com>
2024-02-05 20:42:27 +0100

    feat(API-Nest): Add user signup method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {ConflictException, Injectable} from '@nestjs/common';
 import { signupDTO } from "./DTO/signupDTO";
+import {PrismaService} from "../prisma/prisma.service";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    signup(signupDTO : signupDTO) {
-        return;
+    constructor(private readonly prismaService : PrismaService) {
+    }
+    async signup(signupDTO : signupDTO) {
+        const {email, password, username} = signupDTO;
+        const user = await this.prismaService.user.findUnique({where : {email}});
+        if (user) throw new ConflictException("User already exists.");
+
+        const hash = await bcrypt.hash(password, 10);
+
+        await this.prismaService.user.create({data : {email, username, password : hash}});
+
+        return {data : "User successfully created."};
     }
 }
