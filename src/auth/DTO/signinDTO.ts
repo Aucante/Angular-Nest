d669a5fb14064545e4ce2298ad1f1fc19fb0b05c
Aucante <sebastien.aucante@gmail.com>
2024-02-13 21:13:28 +0100

    feat(API-Nest): Init Swagger

--- src/auth/DTO/signinDTO.ts
+++ src/auth/DTO/signinDTO.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {IsEmail, IsNotEmpty} from "class-validator";
 
 export class signinDTO {
+    @ApiProperty()
     @IsNotEmpty()
     @IsEmail()
     readonly email : string
+    @ApiProperty()
     @IsNotEmpty()
     readonly password : string
 }
--- src/auth/DTO/signupDTO.ts
+++ src/auth/DTO/signupDTO.ts
@@ -1,11 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {IsEmail, IsNotEmpty} from "class-validator"
 
 export class signupDTO {
+    @ApiProperty()
     @IsNotEmpty()
     readonly username : string
+    @ApiProperty()
     @IsNotEmpty()
     @IsEmail()
     readonly email : string
+    @ApiProperty()
     @IsNotEmpty()
     readonly password : string
 }
